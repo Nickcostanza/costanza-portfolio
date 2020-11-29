3fef4cf571617a5fa32f3634c582c97c9b8f225a
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-11-28 18:01:20 -0800

    landing page done

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+      <img src='https://cutewallpaper.org/21/professional-photo-backgrounds/Best-43-Professional-Dark-Background-on-HipWallpaper-Good-.jpg' alt="background"></img>
+      <h1>Nicholas Costanza</h1>
+      <p>Full-Stack Web Developer</p>
+     
+    </div>
+  );
+}
+
+export default Hero;
